c2e59e2281d067cd53c285b826fcfeeb9511e905
Saidamin777 <saidakmalov1707@gmail.com>
2025-04-17 08:27:34 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+
+confirm ('sorovnomada qatnashmoqchisizmi ?')
+
+
+let ism = prompt('Ismingizni kiriting 🧐')
+
+let familya = prompt('Familyangizni kiriting 🧐')
+
+let jins = prompt('Sharifingiz ?🧐')
+
+let yoshingiz = prompt('Yoshingiz ?🧐')
+
+let tel = prompt('Telefon Raqamingiz ?🧐')
+
+let manzil = prompt(' manzilingiz ?🧐')
+
+let tg = prompt('telegramingiz kiriting🧐')
+
+let boy = prompt('Yaxshi korgan oyinchingiz ?🧐')
+
+let oqish = prompt('Maktabingiz raqami ?🧐')
+
+let email = prompt('sizning elektrin pochtangiz ?🧐')
+
+
+console.debug()
+
+
+
+let javob = alert(
+    ' Isming : ' + ism + 
+    ' Familyang : ' + familya + 
+    ' sizning jinsiz: ' + jins + 
+    ' Yoshingiz : ' + yoshingiz +
+    ' telefon raqamingiz : ' + tel +
+    ' Manzilingiz : ' + manzil +
+    ' Telegramingiz  : ' + tg +
+    ' boyingiz: ' + boy +
+    ' Maktabingiz raqami : ' + oqish +
+    ' sizning emailingiz : ' + email
+)
